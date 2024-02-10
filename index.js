27889b757c29229b24da8d064a2d3b156d7cb274
Malik Alabi <marcelalabi776@gmail.com>
2024-02-10 16:09:48 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    var messages = []; 
+    for (var i = 0; i < names.length; i++) {
+        var message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`; 
+        messages.push(message); 
+    }
+    return messages; 
+}
+function countDown(number) {
+    while (number > -1) {
+        console.log(number--);
+    }
+}
